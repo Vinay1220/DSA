73d0f8526fbd6cc9a067f42e793258b810340135
Vinay <vinaypal1220@gmail.com>
2024-02-26 22:37:31 +0530

    Day-37 Missing Number, Reverse Integer

---
+++ 100DayChallenge/Reverse_Integer7.java
@@ -0,0 +1,19 @@
+public class Reverse_Integer7 {
+    public static void main(String[] args) {
+        int x = 648;
+        System.out.println(reverse(x));
+    }
+    public static int reverse(int x) {
+        int rev = 0;
+
+        while(x!=0){
+            int digit = x%10;
+            if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10){
+                return 0;
+            }
+            rev = (rev*10)+digit;
+            x = x/10;
+        }
+        return rev;
+    }
+}
